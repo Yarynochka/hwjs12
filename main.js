d878b2814b8322e263d765f40ea42ca9ffecaf40
Yarynochka <jaryna.games@gmail.com>
2022-05-31 23:15:36 +0300

    changes

--- main.js
+++ main.js
@@ -3,37 +3,37 @@
 // Для кожного елементу свій блок div.post
 // Всі характеристики повинні мати свої блоки всередені div.post
 // https://jsonplaceholder.typicode.com/posts
-
-let posts = document.getElementById('wrap');
-fetch('https://jsonplaceholder.typicode.com/posts')
-    .then(value => value.json())
-    .then(postsDiv =>{
-        for (const post of postsDiv) {
-            let postDiv=document.createElement('div');
-            postDiv.innerText=`${post.id} ${post.title} ${post.body}`;
-            posts.append(postsDiv);
-        }
-    });
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+//
+// let posts = document.getElementById('wrap');
+// fetch('https://jsonplaceholder.typicode.com/posts')
+//     .then(value => value.json())
+//     .then(postsDiv =>{
+//         for (const post of postsDiv) {
+//             let postDiv=document.createElement('div');
+//             postDiv.classList.add('block');
+//             postDiv.innerHTML=` id:${post.id} </br> title:${post.title}</br>  body:${post.body}`;
+//             posts.append(postDiv);
+//         }
+//     });
 
 //     2.
 // Отримати відповідь з цього ресурсу відповідь, та вивести в документ як в прикладі на занятті.
 //     Для кожного елементу свій блок div.comment
 // Всі характеристики повинні мати свої блоки всередені div.comment
 // https://jsonplaceholder.typicode.com/comments
+
+let comments =document.getElementById('wrap');
+fetch('https://jsonplaceholder.typicode.com/comments')
+.then(value => value.json())
+.then(commentsDiv=>{
+    for (const comment of commentsDiv) {
+        let commentDiv=document.createElement('div');
+        commentDiv.classList.add('comment');
+        comments.append(commentDiv);
+        for (const elementItem in comment) {
+          const divElement= document.createElement('div');
+          divElement.innerText=`${elementItem} ${comment[elementItem]}`;
+          comments.append(divElement);
+        }
+    }
+});
